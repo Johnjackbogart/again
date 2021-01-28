cc5e91f0e6a65d324172fb5a0bf6d7722322af4e
John Bogart <johnjackbogart@gmail.com>
2021-01-27 23:57:31 -0500

    added some middleware, some routes, api calls that mean nothing, going to work tomorrow

--- first/back/server.js
+++ first/back/server.js
@@ -2,11 +2,22 @@
 //honestly just commenting to get in the hang of comments
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+
+//middleware has to go in front of the routes i think because of how node handles stuff
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //i think if I remember this returns just heyo when at root url
 app.get('/', (req, res) => {
 	res.send('heyo');
+});
+
+//accepts user info and returns it
+app.post("/user", (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
 });
 
 //this will log the message to the terminal when port 5000 is active I think?
