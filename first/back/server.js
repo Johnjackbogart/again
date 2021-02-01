9c5f5e810098a1fbbf7d8b909bed7a22c2696200
John Bogart <johnjackbogart@gmail.com>
2021-02-01 10:46:58 -0500

    is ssh working?

--- first/back/server.js
+++ first/back/server.js
@@ -4,10 +4,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+require('dotenv').config(); //allows us to load environment variables
+
+//literally just going to add some comments to test if my ssh agent is working
 
 //middleware has to go in front of the routes i think because of how node handles stuff
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//set up mongo connection to db
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+	.connect(MONGO_URI, {useNewUrlParser: true})
+	.then(()=> console.log('connected'))
+	.catch(e => console.log('error, not connected'));
 
 //i think if I remember this returns just heyo when at root url
 app.get('/', (req, res) => {
